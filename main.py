801c54962576bdd8453b6044a28146fbbce667e4
andre_zktt <andrezicatti@gmail.com>
2025-09-17 12:06:27 -0300

    :sparkles: feat: create data processing method, treating NaN's and creating more columns

--- main.py
+++ main.py
@@ -17,7 +17,23 @@ def load_data():
     df = pd.read_csv("./data/sao_paulo_imoveis_2019.csv")
     return df
 
+def data_processing(df):
+
+    for col in ["Condo", "Suites", "Parking", "Toilets"]:
+        df[col] = df[col].fillna(0).astype(int)
+
+    for col in ["Elevator", "Furnished", "Swimming Pool", "New"]:
+        df[col] = df[col].fillna(0).astype(int)
+
+    df.dropna(subset=["Negotiation Type"], inplace=True)
+
+    df["Price_m2"] = (df["Price"] / df["Size"]).round(2)
+    df["Total_cost"] = df["Price"] + df["Condo"]
+
+    return df
+
 df_raw = load_data()
+df = data_processing(df_raw)
 
 st.subheader("Dados Brutos")
-st.dataframe(df_raw.head())
+st.dataframe(df.sample(50))
