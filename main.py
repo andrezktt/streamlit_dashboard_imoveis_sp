adb23a3d42bc1ef202b6baa1594bdfdc16d23297
andre_zktt <andrezicatti@gmail.com>
2025-09-17 11:51:39 -0300

    :sparkles: feat: create load data method that read csv files and return dataset

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
+st.set_page_config(layout="wide")
+
+st.title("🏙️ Análise de Imóveis em São Paulo")
+
+st.markdown("""
+Este dashboard interativo permite explorar e analisar dados de imóveis à venda na cidade de São Paulo.
+Use filtros na barra lateral para segmentar os dados por tipo, bairro, características e preço.
+"""
+)
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv("./data/sao_paulo_imoveis_2019.csv")
+    return df
+
+df_raw = load_data()
+
+st.subheader("Dados Brutos")
+st.dataframe(df_raw.head())
